b06a3d93abb23c22e1635808fb44d28f2452787d
lazarenkoal <avlazarenko@edu.hse.ru>
2016-03-27 21:30:22 +0300

    Major bug fixed

--- shitter2.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'aleksandrlazarenko'
-import xmltodict
-import program_data_manager
-
-print(program_data_manager.upload_program_data())
-program_data_manager.update_token('aaa')
-program_data_manager.update_spent_money(10)
-print(program_data_manager.upload_program_data())
